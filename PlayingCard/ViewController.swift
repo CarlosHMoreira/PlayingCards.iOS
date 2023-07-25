1f0749ae6e3abd27d67b937adcca0c59cda07f59
Carlos Henrique da Silva Moreira <kmoreira620@gmail.com>
2023-07-24 23:59:02 -0300

    Initial Commit

---
+++ PlayingCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by Carlos Henrique on 24/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
