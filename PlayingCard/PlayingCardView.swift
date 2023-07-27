ff65878ce571b00b906472d159b8093193f5b600
Carlos Henrique da Silva Moreira <kmoreira620@gmail.com>
2023-07-27 00:32:03 -0300

    Example of drawing with BezierPath

--- PlayingCard/PlayingCardView.swift
+++ PlayingCard/PlayingCardView.swift
@@ -13,6 +13,10 @@ class PlayingCardView: UIView {
 //     Only override draw() if you perform custom drawing.
 //     An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
+        arcWithBezierPath()
+    }
+    
+    private func arcWithContext() {
         if let context = UIGraphicsGetCurrentContext() {
             context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
             context.setLineWidth(5.0)
@@ -21,6 +25,16 @@ class PlayingCardView: UIView {
             context.strokePath()
             context.fillPath()
         }
+    }
+    
+    private func arcWithBezierPath() {
+        let path = UIBezierPath()
+        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+        path.lineWidth = 5.0
+        UIColor.green.setFill()
+        UIColor.red.setStroke()
+        path.stroke()
+        path.fill()
     }
     
 
